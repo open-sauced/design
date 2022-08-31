46b70bf76ad24196dd92a3e01d8ffd9a8a0d4aff
smith <oliviamegan11@gmail.com>
2022-08-31 11:07:52 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ const withImages = require('next-images');
 const withTM = require('next-transpile-modules')(['@br-sauced/design-system']);
 
 /** @type {import('next').NextConfig} */
-module.exports = withTM(withImages({
-  reactStrictMode: true,
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-    disableStaticImages: true,
-  },
-}));
+module.exports = withTM(
+  withImages({
+    reactStrictMode: true,
+    images: {
+      domains: ['avatars.githubusercontent.com', 'images.unsplash.com'],
+      disableStaticImages: true,
+    },
+  })
+);
