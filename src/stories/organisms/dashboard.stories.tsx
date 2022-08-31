d9e3013c6badfab3ff941b0c23fdb5592acd758d
smith <oliviamegan11@gmail.com>
2022-08-31 17:25:00 +0100

    Migrated organisms stories, next is run through code to see possible refactor

---
+++ src/stories/organisms/dashboard.stories.tsx
@@ -0,0 +1,8 @@
+import Dashboard from 'src/components/organisms/Dashboard/dashboard';
+
+const StoryConfig = {
+  title: 'Design System/organisms/Dashboard',
+};
+export default StoryConfig;
+
+export const DashboardOrganism = (): JSX.Element => <Dashboard />;
