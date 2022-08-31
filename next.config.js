516a2287878013b52498743509e36a47609c8976
smith <oliviamegan11@gmail.com>
2022-08-31 11:12:43 +0100

    Migrated all atoms sucessfully, moving to molecules

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withImages = require('next-images');
-const withTM = require('next-transpile-modules')(['@br-sauced/design-system']);
+const withImages = require("next-images");
+const withTM = require("next-transpile-modules")(["@br-sauced/design-system"]);
 
 /** @type {import('next').NextConfig} */
 module.exports = withTM(
   withImages({
     reactStrictMode: true,
     images: {
-      domains: ['avatars.githubusercontent.com', 'images.unsplash.com'],
-      disableStaticImages: true,
-    },
+      domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+      disableStaticImages: true
+    }
   })
 );
