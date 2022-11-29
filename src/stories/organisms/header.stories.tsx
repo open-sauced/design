ab11c0b5a9ec607a426c299eb4c2b32b6c433289
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-11-29 11:56:51 -0500

    refactor(removing molecules and organisms): only atoms
    only atoms.

--- src/stories/organisms/header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../components/organisms/header/header";
-
-const storyConfig = {
-  title: "Design System/Organisms/Header"
-};
-
-export default storyConfig;
-
-export const HeaderOrganism = () => <Header />;
