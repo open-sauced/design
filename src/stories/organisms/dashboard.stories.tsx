b3b8b513473bfb5efefaf750748d80a3266e4e42
Janie Lee <janie.lee.inquire@gmail.com>
2022-12-02 13:36:28 -0500

    refactor(removing molecules and organisms): only atoms (#12)
    only atoms.

--- src/stories/organisms/dashboard.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Dashboard from 'src/components/organisms/Dashboard/dashboard';
-
-const StoryConfig = {
-  title: 'Design System/organisms/Dashboard',
-};
-export default StoryConfig;
-
-export const DashboardOrganism = (): JSX.Element => <Dashboard />;
